72e965c2c45d8d825da2c5f38ec39e8c5b879eb0
dhosio <d.githiomi@alustudent.com>
2022-04-17 02:23:07 +0400

    Added feedback to the questions

--- JS/quizpage.js
+++ JS/quizpage.js
@@ -6,13 +6,13 @@ const QUESTION_SCORE = 1;
 // Select all div choices
 let allChoiceDivs = document.querySelectorAll('label[class~=optionContainer]')
 
-    // Set a click handler on each div
-    for (let choiceDiv of allChoiceDivs){
-        choiceDiv.onclick = choiceClickHandler;
-    }
+// Set a click handler on each div
+for (let choiceDiv of allChoiceDivs) {
+    choiceDiv.onclick = choiceClickHandler;
+}
 
 // For the Choice Click Handler
-function choiceClickHandler(event){
+function choiceClickHandler(event) {
 
     let currentDiv = event.currentTarget;
 
@@ -21,14 +21,14 @@ function choiceClickHandler(event){
     let children = parent.children;
 
     // Remove only previously selected option from all except checkboxes
-    if ( questionNumber !== 2 ){
+    if (questionNumber !== 2) {
 
-        for (let child of children){
-            if (child.className === "optionContainer"){
+        for (let child of children) {
+            if (child.className === "optionContainer") {
                 child.removeAttribute("style");
             }
         }
-        
+
     }
 
     // To set the background color of the selected option
@@ -43,73 +43,65 @@ function choiceClickHandler(event){
 
 // This is for the drag and drop functionality
 
-    // Get all the draggable items
-    const draggableElements = document.querySelectorAll('.draggableContainer');
+// Get all the draggable items
+const draggableElements = document.querySelectorAll('.draggableContainer');
 
-    // To get the drop zone
-    const dropZone = document.querySelector('#dropLocation')
+// To get the drop zone
+const dropZone = document.querySelector('#dropLocation')
 
-    // Then add an event listener
-    for ( const draggableElement of draggableElements ){
+// Then add an event listener
+for (const draggableElement of draggableElements) {
 
-        draggableElement.addEventListener('dragstart', event => {
-            event.dataTransfer.setData("text", draggableElement.id)
-        })
+    draggableElement.addEventListener('dragstart', event => {
+        event.dataTransfer.setData("text", draggableElement.id)
+    })
 
-        // Adding drag over event listener
-        dropZone.addEventListener("dragover", e => {
-            // Prevent default behaviour
-            e.preventDefault();
+    // Adding drag over event listener
+    dropZone.addEventListener("dragover", e => {
+        // Prevent default behaviour
+        e.preventDefault();
 
-            // Adding the style class to the drop zone
-            dropZone.classList.add("dragZone--over");
-        })
+        // Adding the style class to the drop zone
+        dropZone.classList.add("dragZone--over");
+    })
 
-        // When the item is not in the drag zone
-        dropZone.addEventListener("dragleave", e => {
+    // When the item is not in the drag zone
+    dropZone.addEventListener("dragleave", e => {
+        dropZone.classList.remove("dragZone--over");
+    })
+
+    // Adding the drop listener
+    dropZone.addEventListener("drop", e => {
+        // Prevent default behaviour
+        e.preventDefault();
+
+        // To actually move the element
+        const droppedElementId = e.dataTransfer.getData("text");
+        const droppedElement = document.getElementById(droppedElementId);
+
+        if (dropZone.child == null) {
+            dropZone.appendChild(droppedElement);
             dropZone.classList.remove("dragZone--over");
-        })
+        } else {
+            dropZone.replaceChild(droppedElement);
+        }
 
-        // Adding the drop listener
-        dropZone.addEventListener("drop", e => {
-            // Prevent default behaviour
-            e.preventDefault();
+    })
 
-            // To actually move the element
-            const droppedElementId = e.dataTransfer.getData("text");
-            const droppedElement = document.getElementById(droppedElementId);
+}
 
-            if (dropZone.child == null){
-                dropZone.appendChild(droppedElement);
-                dropZone.classList.remove("dragZone--over");
-            }else {
-                dropZone.replaceChild(droppedElement);
-            }
-            
-        })
+// Setting an event listener to the 'Check' Button
+let checkButtons = document.querySelectorAll('.checkButton');
 
-    }
+// Event Listener
+for (let checkButton of checkButtons) {
 
-
-// Setting an event listener to the 'Next' Button
-let nextButton = document.querySelector('#nextButton');
-
-    // Variable to hold the question number
-    let questionNumber = 1;
-    let score = 0;
-
-    // HUD to show user progress
-    let progressText = document.querySelector('#progressText')
-    let progressBar = document.querySelector('#progressBarFull')
-    let scoreText = document.querySelector('#score')
-
-    // Adding the event listener
-    nextButton.addEventListener('click', (event) => {
-
-        // Prevent page from reloading
+    checkButton.addEventListener('click', event => {
+        // Prevent refreshing
         event.preventDefault();
 
-        if ( !choiceSelected ){
+        // To check the answers put by the user
+        if (!choiceSelected) {
 
             // Sweet Alert to show choice not selected for all except question 3
             swal({
@@ -118,127 +110,181 @@ let nextButton = document.querySelector('#nextButton');
                 icon: 'error'
             })
 
-        }else {
+        } else {
 
-            if (questionNumber === 1){
+            // Hide the check button
+            checkButton.setAttribute("hidden", "true");
 
-                // Get the answer selected by the user
-                // const currentForm = document.forms.qn1ChoiceForm;
-                // const chosenAnswer = currentForm.elements.choice.value;
+            // Show the Feedback Container and the next button
+            const feedbackContainer = document.querySelector(`#feedback${questionNumber}`);
+            feedbackContainer.removeAttribute("hidden");
 
-                const answer = document.querySelector('input[name="choice"]:checked').value;
-                
-                // Check if the chossen answer is correct
-                let correctAnswer = "C"
+        }
+    })
+}
 
-                if ( answer === correctAnswer ){
-                    alert("Correct")
-                }else{
-                    alert("Incorrect")
-                }
 
-            }else if (questionNumber === 2){
+// Setting an event listener to the 'Next' Button
+let nextButton = document.querySelector('#nextButton');
 
-                // To get the checkboxes by their name
-                const allCheckboxes = document.getElementsByName('chbxChoice');
+// Variable to hold the question number
+let questionNumber = 1;
+let score = 0;
 
-                // Loop through each checkbox and get the value
-                const checkboxValues = [];
+// HUD to show user progress
+let progressText = document.querySelector('#progressText')
+let progressBar = document.querySelector('#progressBarFull')
+let scoreText = document.querySelector('#score')
 
-                for ( let index = 0; index < allCheckboxes.length; index++ ){
-                    // Check if the checkbox is checked
-                    const checkbox = allCheckboxes[index];
-                    if ( checkbox.checked === true ){
-                        const value = checkbox.value;
-                        // Add value to the array
-                        checkboxValues.push( value.toString() );
-                    }
-                }
+// Adding the event listener
+nextButton.addEventListener('click', (event) => {
 
-                // Correct checkbox values
-                const correctCheckboxValues = ["A", "C", "D"];
+    // Prevent page from reloading
+    event.preventDefault();
 
-                if (JSON.stringify(checkboxValues) == JSON.stringify(correctCheckboxValues)) {
-                    alert("Correct");
-                }else {
-                    alert("Incorrect");
-                }
+    if (!choiceSelected) {
 
-            }else if ( questionNumber === 3 ){
+        // Sweet Alert to show choice not selected for all except question 3
+        swal({
+            title: 'Error!',
+            text: 'You must select an answer before proceeding',
+            icon: 'error'
+        })
 
-                // Get the value of the choice choice selected
-                const inputAnswer = document.getElementById('inputAnswer').value;
-                alert(inputAnswer);
+    } else {
 
-                const correctAnswer1 = "Dhosio";
-                const correctAnswer2 = "dhosio";
+        if (questionNumber === 1) {
 
-                if ( inputAnswer === correctAnswer1 || inputAnswer === correctAnswer2 ){
-                    alert("Correct")
-                }else {
-                    alert("Incorrect")
-                }
-                
-            }else if ( questionNumber === 4 ){
+            // Get the answer selected by the user
+            // const currentForm = document.forms.qn1ChoiceForm;
+            // const chosenAnswer = currentForm.elements.choice.value;
 
-                // To get the selected drop down option
-                const dropDownOption = document.querySelector('#dropDown').value;
-                alert(dropDownOption);
+            const answer = document.querySelector('input[name="choice"]:checked').value;
 
-                // Check if the answer is correct
-                const correctDropDownAnswer = "Daniel";
-                
-                if ( dropDownOption === correctDropDownAnswer){
-                    alert("Correct")
-                }else {
-                    alert("Incorrect")
-                }
+            // Check if the chossen answer is correct
+            let correctAnswer = "Dhosio Regency"
 
+            if (answer === correctAnswer) {
+                // Increment score
+                ++score;
+            } else {
+                alert("Incorrect")
             }
 
-            // Get the div that contains the current question
-            const currentQuestionDiv = document.getElementById(`qn${questionNumber}`);
-            // Hide the current question
-            currentQuestionDiv.setAttribute("hidden", "true");
+        } else if (questionNumber === 2) {
 
-            // Reset choice selected to false
-            choiceSelected = false;
+            // To get the checkboxes by their name
+            const allCheckboxes = document.getElementsByName('chbxChoice');
 
-            // Increment the questions counter
-            questionNumber++;
+            // Loop through each checkbox and get the value
+            const checkboxValues = [];
 
-            // To check if the number of questions are depleted
-            if ( questionNumber === ( MAX_QUESTIONS + 1 ) ){
+            for (let index = 0; index < allCheckboxes.length; index++) {
+                // Check if the checkbox is checked
+                const checkbox = allCheckboxes[index];
+                if (checkbox.checked === true) {
+                    const value = checkbox.value;
+                    // Add value to the array
+                    checkboxValues.push(value.toString());
+                }
+            }
 
-                // Show an alert to end quiz
-                swal({
-                    title: 'Congratualtions!',
-                    text: 'You have successfully completed the Quiz',
-                    icon: 'success',
-                    button: 'View Results'
-                })
+            // Correct checkbox values
+            const correctCheckboxValues = ["Lifestyle Management", "Corporate Concierge", "Private Touring"];
 
-                // Hide the Quiz container
-                const quizContainer = document.querySelector('.quizContainer');
-                quizContainer.setAttribute("hidden", "true");
+            if (JSON.stringify(checkboxValues) == JSON.stringify(correctCheckboxValues)) {
+                // Increment score
+                ++score;
+            } else {
+                alert("Incorrect");
+            }
 
-                // Show the Results Container
-                const resultContainer = document.getElementById("rs1");
-                resultContainer.removeAttribute("hidden");
+        } else if (questionNumber === 3) {
+
+            // Get the value of the choice choice selected
+            const inputAnswerContainer = document.querySelector('#inputAnswer');
+            const inputAnswer = inputAnswerContainer.value;
+
+            // Check if inputAnswer is null
+            if (inputAnswer === "") {
+
+                // Set style to highlight error
+                alert(inputAnswer)
+                inputAnswerContainer.style.borderColor = '2px dashed red';
+                return
 
             } else {
 
-                // To alter the HUD and show user progress
-                progressText.innerText = `Question ${questionNumber} of ${MAX_QUESTIONS}`
-                progressBar.style.width = `${ (questionNumber/MAX_QUESTIONS) * 100 }%`
-                scoreText.innerText = ++score;
+                const correctAnswer1 = "comfort";
+                const correctAnswer2 = "Comfort";
 
-                // Reveal the next question
-                const nextQuestionDiv = document.getElementById(`qn${questionNumber}`);
-                nextQuestionDiv.removeAttribute("hidden");
+                if (inputAnswer === correctAnswer1 || inputAnswer === correctAnswer2) {
+                    // Increment score
+                    ++score;
+                } else {
+                    alert("Incorrect")
+                }
 
             }
 
+        } else if (questionNumber === 4) {
+
+            // To get the selected drop down option
+            const dropDownOption = document.querySelector('#dropDown').value;
+
+            // Check if the answer is correct
+            const correctDropDownAnswer = "Nairobi City";
+
+            if (dropDownOption === correctDropDownAnswer) {
+                // Increment Score
+                ++score;
+            } else {
+                alert("Incorrect")
+            }
+
+        } else {
+
+            // Code for question 5
+
         }
 
-    })
+        // Get the div that contains the current question
+        const currentQuestionDiv = document.getElementById(`qn${questionNumber}`);
+        // Hide the current question
+        currentQuestionDiv.setAttribute("hidden", "true");
+
+        // Reset choice selected to false
+        choiceSelected = false;
+
+        // Increment the questions counter
+        questionNumber++;
+
+        // To check if the number of questions are depleted
+        if (questionNumber === (MAX_QUESTIONS + 1)) {
+
+            // Show an alert to end quiz
+            swal({
+                title: 'Congratualtions!',
+                text: 'You have successfully completed the Quiz',
+                icon: 'success',
+                button: 'View Results'
+            })
+
+            // Hide the Quiz container
+            const quizContainer = document.querySelector('.quizContainer');
+            quizContainer.setAttribute("hidden", "true");
+
+        } else {
+
+            // To alter the HUD and show user progress
+            progressText.innerText = `Question ${questionNumber} of ${MAX_QUESTIONS}`
+            progressBar.style.width = `${(questionNumber / MAX_QUESTIONS) * 100}%`
+            scoreText.innerText = score;
+
+            // Reveal the next question
+            const nextQuestionDiv = document.getElementById(`qn${questionNumber}`);
+            nextQuestionDiv.removeAttribute("hidden");
+
+        }
+    }
+})

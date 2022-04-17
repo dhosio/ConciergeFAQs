9aba0538e466158169ca18db8d66d2ac04edd3ed
dhosio <d.githiomi@alustudent.com>
2022-04-17 22:36:03 +0400

    Completed Quiz page with all working JS

--- JS/quizpage.js
+++ JS/quizpage.js
@@ -2,6 +2,10 @@
 let choiceSelected = false;
 const MAX_QUESTIONS = 5;
 const QUESTION_SCORE = 1;
+// Styling colors
+let wrongBackgroundColor = '#c50505'; //Red
+let correctBackgroundColor = '#7cb918'; //Green
+
 
 // Select all div choices
 let allChoiceDivs = document.querySelectorAll('label[class~=optionContainer]')
@@ -32,9 +36,8 @@ function choiceClickHandler(event) {
     }
 
     // To set the background color of the selected option
-    let correctBackgroundcolor = '#7cb918'; //Green
-    let wrongBackgroundcolor = '#c50505'; //Red
-    currentDiv.setAttribute("style", `background-color: ${correctBackgroundcolor}; color: white; font-weight:bold;`);
+    let selectedBackgroundcolor = '#50473ed2'; //Dark grey
+    currentDiv.setAttribute("style", `background-color: ${selectedBackgroundcolor}; color: white; font-weight:bold;`);
 
     // Set choice selected to true
     choiceSelected = true;
@@ -91,7 +94,11 @@ for (const draggableElement of draggableElements) {
 }
 
 // Declaring the 'Next' Button
-let nextButton = document.querySelector('#nextButtonContainer');
+let nextButton = document.querySelector('#nextButton');
+
+// Variable to hold the question number and score
+let questionNumber = 1;
+let score = 0;
 
 // Setting an event listener to the 'Check' Button
 let checkButtons = document.querySelectorAll('.checkButton');
@@ -106,14 +113,139 @@ for (let checkButton of checkButtons) {
         // To check the answers put by the user
         if (!choiceSelected) {
 
-            // Sweet Alert to show choice not selected for all except question 3
+            // Sweet Alert to show choice not selected
             swal({
                 title: 'Error!',
-                text: 'You must select an answer before proceeding',
-                icon: 'error'
+                text: 'You must select at least one choice before proceeding',
+                icon: 'error',
+                button: 'Try again'
             })
 
         } else {
+
+            if (questionNumber === 1) {
+
+                // Get the answer selected by the user
+                // const currentForm = document.forms.qn1ChoiceForm;
+                // const chosenAnswer = currentForm.elements.choice.value;
+
+                // Getting the value of the selected radio button
+                const answer = document.querySelector('input[name="choice"]:checked')
+                const answerValue = answer.value;
+
+                // Check if the chossen answer is correct
+                let correctAnswer = "Dhosio Regency"
+
+                if (answerValue === correctAnswer) {
+                    // Highlight selected choice
+                    answer.parentElement.parentElement.classList.add('correct')
+                    // Increment score
+                    ++score;
+                } else {
+                    answer.parentElement.parentElement.classList.add('incorrect')
+                }
+
+            } else if (questionNumber === 2) {
+
+                // To get the checkboxes by their name
+                const allCheckboxes = document.getElementsByName('chbxChoice');
+
+                // Correct checkbox values
+                const correctCheckboxValues = ["Lifestyle Management", "Corporate Concierge", "Private Touring"];
+                // Loop through each checkbox and get the value
+                const checkboxValues = [];
+
+                for (let index = 0; index < allCheckboxes.length; index++) {
+                    // Check if the checkbox is checked
+                    const checkbox = allCheckboxes[index];
+                    const correctAnswer = correctCheckboxValues[index];
+
+                    if (checkbox.checked === true) {
+                        const value = checkbox.value;
+                        // Compare the value to the correct answer
+                        if (value === correctAnswer) {
+                            checkbox.parentElement.parentElement.classList.add('correct');
+                            // Add value to the array
+                            checkboxValues.push(value.toString());
+                        } else {
+                            checkbox.parentElement.parentElement.classList.add('incorrect');
+                        }
+                    }
+                }
+
+                // Check if all answer match the correct answers
+                if (JSON.stringify(checkboxValues) === JSON.stringify(correctCheckboxValues)) {
+                    // Increment score
+                    ++score;
+                } else {
+
+                }
+
+            } else if (questionNumber === 3) {
+
+                // Get the value of the checked item
+                let answer = document.querySelector('input[name="inputChoice"]:checked');
+                const answerValue = answer.value;
+
+                // Get the input container and put the answer selected
+                const inputAnswerContainer = document.getElementById('inputAnswer');
+                inputAnswerContainer.value = answerValue;
+
+                // Confirm that answer is correct
+                const correctAnswer = "Comfort";
+                if (answerValue === correctAnswer) {
+                    answer.parentElement.parentElement.classList.add('correct');
+                    // Then increment score
+                    ++score;
+                } else {
+                    answer.parentElement.parentElement.classList.add('incorrect');
+                }
+
+            } else if (questionNumber === 4) {
+
+                // To get the selected drop down option
+                const dropDown = document.querySelector('#dropDown');
+                const dropDownValue = dropDown.value;
+
+                // Check if the answer is correct
+                const correctDropDownAnswer = "Nairobi City";
+
+                if (dropDownValue === correctDropDownAnswer) {
+                    // Make parent element the correct answer
+                    dropDown.classList.add('correct');
+                    dropDown.parentElement.classList.add('correct');
+                    // Increment Score
+                    ++score;
+                } else {
+                    // Make parent element incorrect answer
+                    dropDown.classList.add('incorrect');
+                    dropDown.parentElement.classList.add('incorrect');
+                }
+
+            } else if (questionNumber === 5) {
+
+                // Getting the value of the selected radio button
+                const dropAnswer = document.querySelector('input[name="dropChoice"]:checked')
+                const dropAnswerValue = dropAnswer.value;
+
+                // Check if the chossen answer is correct
+                let correctDropAnswer = "Snapchat";
+
+                if (dropAnswerValue === correctDropAnswer) {
+                    // Highlight selected choice
+                    dropAnswer.parentElement.parentElement.classList.add('correct')
+                    // Increment score
+                    ++score;
+                } else {
+                    dropAnswer.parentElement.parentElement.classList.add('incorrect')
+                }
+
+            } else {
+
+                // End all
+                return
+
+            }
 
             // Hide the check button
             checkButton.setAttribute("hidden", "true");
@@ -128,10 +260,6 @@ for (let checkButton of checkButtons) {
     })
 }
 
-// Variable to hold the question number
-let questionNumber = 1;
-let score = 0;
-
 // HUD to show user progress
 let progressText = document.querySelector('#progressText')
 let progressBar = document.querySelector('#progressBarFull')
@@ -143,152 +271,52 @@ nextButton.addEventListener('click', (event) => {
     // Prevent page from reloading
     event.preventDefault();
 
-    if (!choiceSelected) {
+    // Get the div that contains the current question
+    const currentQuestionDiv = document.getElementById(`qn${questionNumber}`);
+    // Hide the current question
+    currentQuestionDiv.setAttribute("hidden", "true");
 
-        // Sweet Alert to show choice not selected for all except question 3
+    // Reset choice selected to false
+    choiceSelected = false;
+
+    // Update the score
+    scoreText.innerText = score;
+
+    // Increment the questions counter
+    questionNumber++;
+
+    // To alter the HUD and show user progress
+    progressText.innerText = `Question ${questionNumber} of ${MAX_QUESTIONS}`
+    progressBar.style.width = `${(questionNumber / MAX_QUESTIONS) * 100}%`
+
+    // To check if the number of questions are depleted
+    if (questionNumber === (MAX_QUESTIONS + 1)) {
+
+        // Change question progress text to maximum
+        progressText.innerText = `Question ${MAX_QUESTIONS} of ${MAX_QUESTIONS}`
+
+        // Show an alert to end quiz
         swal({
-            title: 'Error!',
-            text: 'You must select an answer before proceeding',
-            icon: 'error'
+            title: 'Congratualtions!',
+            text: 'You have successfully completed the Quiz',
+            icon: 'success',
+            button: 'View Results'
         })
 
-    } else {
+        // Hide the Quiz container
+        const quizContainer = document.querySelector('.quizContainer');
+        quizContainer.setAttribute("hidden", "true");
 
-        if (questionNumber === 1) {
+        // End all
+        return;
 
-            // Get the answer selected by the user
-            // const currentForm = document.forms.qn1ChoiceForm;
-            // const chosenAnswer = currentForm.elements.choice.value;
-
-            const answer = document.querySelector('input[name="choice"]:checked').value;
-
-            // Check if the chossen answer is correct
-            let correctAnswer = "Dhosio Regency"
-
-            if (answer === correctAnswer) {
-                // Increment score
-                ++score;
-            } else {
-                alert("Incorrect")
-            }
-
-        } else if (questionNumber === 2) {
-
-            // To get the checkboxes by their name
-            const allCheckboxes = document.getElementsByName('chbxChoice');
-
-            // Loop through each checkbox and get the value
-            const checkboxValues = [];
-
-            for (let index = 0; index < allCheckboxes.length; index++) {
-                // Check if the checkbox is checked
-                const checkbox = allCheckboxes[index];
-                if (checkbox.checked === true) {
-                    const value = checkbox.value;
-                    // Add value to the array
-                    checkboxValues.push(value.toString());
-                }
-            }
-
-            // Correct checkbox values
-            const correctCheckboxValues = ["Lifestyle Management", "Corporate Concierge", "Private Touring"];
-
-            if (JSON.stringify(checkboxValues) == JSON.stringify(correctCheckboxValues)) {
-                // Increment score
-                ++score;
-            } else {
-                alert("Incorrect");
-            }
-
-        } else if (questionNumber === 3) {
-
-            // Get the value of the choice choice selected
-            const inputAnswerContainer = document.querySelector('#inputAnswer');
-            const inputAnswer = inputAnswerContainer.value;
-
-            // Check if inputAnswer is null
-            if (inputAnswer === "") {
-
-                // Set style to highlight error
-                alert(inputAnswer)
-                inputAnswerContainer.style.borderColor = '2px dashed red';
-                return
-
-            } else {
-
-                const correctAnswer1 = "comfort";
-                const correctAnswer2 = "Comfort";
-
-                if (inputAnswer === correctAnswer1 || inputAnswer === correctAnswer2) {
-                    // Increment score
-                    ++score;
-                } else {
-                    alert("Incorrect")
-                }
-
-            }
-
-        } else if (questionNumber === 4) {
-
-            // To get the selected drop down option
-            const dropDownOption = document.querySelector('#dropDown').value;
-
-            // Check if the answer is correct
-            const correctDropDownAnswer = "Nairobi City";
-
-            if (dropDownOption === correctDropDownAnswer) {
-                // Increment Score
-                ++score;
-            } else {
-                alert("Incorrect")
-            }
-
-        } else {
-
-            // Code for question 5
-
-        }
-
-        // Get the div that contains the current question
-        const currentQuestionDiv = document.getElementById(`qn${questionNumber}`);
-        // Hide the current question
-        currentQuestionDiv.setAttribute("hidden", "true");
-
-        // Reset choice selected to false
-        choiceSelected = false;
-
-        // Increment the questions counter
-        questionNumber++;
-
-        // To check if the number of questions are depleted
-        if (questionNumber === (MAX_QUESTIONS + 1)) {
-
-            // Show an alert to end quiz
-            swal({
-                title: 'Congratualtions!',
-                text: 'You have successfully completed the Quiz',
-                icon: 'success',
-                button: 'View Results'
-            })
-
-            // Hide the Quiz container
-            const quizContainer = document.querySelector('.quizContainer');
-            quizContainer.setAttribute("hidden", "true");
-
-        } else {
-
-            // To alter the HUD and show user progress
-            progressText.innerText = `Question ${questionNumber} of ${MAX_QUESTIONS}`
-            progressBar.style.width = `${(questionNumber / MAX_QUESTIONS) * 100}%`
-            scoreText.innerText = score;
-
-            // Reveal the next question
-            const nextQuestionDiv = document.getElementById(`qn${questionNumber}`);
-            nextQuestionDiv.removeAttribute("hidden");
-
-            // Re-hide the next button
-            nextButton.setAttribute("hidden", "true");
-
-        }
     }
+
+    // Reveal the next question
+    const nextQuestionDiv = document.getElementById(`qn${questionNumber}`);
+    nextQuestionDiv.removeAttribute("hidden");
+
+    // Re-hide the next button
+    nextButton.setAttribute("hidden", "true");
+
 })
